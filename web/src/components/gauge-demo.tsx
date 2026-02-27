504a6989947df55de38f6870e12733d5a760c370
Brendon Robinson <brobinson9999@gmail.com>
2026-02-27 13:20:47 -0500

    More gauge styling.

--- web/src/components/gauge-demo.tsx
+++ web/src/components/gauge-demo.tsx
@@ -22,6 +22,11 @@ export const GaugeDemo: React.FC<GaugeDemoProps> = ({ className }) => {
 
   return (
     <div className={className}>
+      <Gauge label={`0`} indicatorPosition={0} />
+      <Gauge label={`25`} indicatorPosition={0.25} />
+      <Gauge label={`50`} indicatorPosition={0.5} />
+      <Gauge label={`75`} indicatorPosition={0.75} />
+      <Gauge label={`100`} indicatorPosition={1} />
       <Gauge label={`${value}`} indicatorPosition={indicatorPosition} />
     </div>
   );
